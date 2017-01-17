98f9881d20dfbeefc96d0f1b84167bdb90fbaf09
Michael Driscoll <mdriscoll@pax8.com>
2017-01-16 20:52:49 -0700

    Everything is utf-8, and stop trying to disable user zoom

--- index.php
+++ index.php
@@ -60,6 +60,7 @@ EOT;
         ?>
             <html>
             <head>
+            <meta charset="utf-8">
             <meta name="viewport" content="width=800">
             <title>Customer <?=$cid?> Export Progress</title>
             </head>
@@ -86,8 +87,8 @@ EOT;
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
     crossorigin="anonymous">
-<meta name="viewport" content="width=device-width, initial-scale=1,
-                               maximum-scale=1, user-scalable=no">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="format-detection" content="telephone=no">
 <style>
 .inset {
